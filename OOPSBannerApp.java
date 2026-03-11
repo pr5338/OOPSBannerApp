74a5288cbcb6cb145c0925413872dabadc1b96a5
pr5338 <pr5338@srmist.edu.in>
2026-03-11 23:45:33 +0530

    printing oops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -110,3 +110,63 @@ public class OOPSBannerApp{
         printMessage(message, charMaps);
     }
 }
+
+
+
+public class OOPSBannerApp {
+
+    public static HashMap<Character, String[]> createCharacterMap() {
+
+        HashMap<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        map.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        map.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        return map;
+    }
+
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < message.length(); j++) {
+
+                char ch = message.charAt(j);
+                String[] pattern = charMap.get(ch);
+
+                System.out.print(pattern[i] + "  ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+        displayBanner(message, charMap);
+
+    }
+}
