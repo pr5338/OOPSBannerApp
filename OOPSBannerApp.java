51f6ee29dc742e52d195f18a267818b34fb14ab1
pr5338 <pr5338@srmist.edu.in>
2026-03-11 23:30:01 +0530

    printing oops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,58 +1,112 @@
-public class OOPSBannerApp {
 
-    // Method to generate O pattern
-    public static String[] getOPattern() {
-        return new String[]{
-                " *** ",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*   *",
-                " *** "
-        };
+public class OOPSBannerApp{
+    static class CharacterPatternMap {
+        char character;
+        String[] pattern;
+
+        CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        char getCharacter() {
+            return character;
+        }
+
+        String[] getPattern() {
+            return pattern;
+        }
     }
 
-    // Method to generate P pattern
-    public static String[] getPPattern() {
-        return new String[]{
-                "***** ",
-                "*    *",
-                "*    *",
-                "***** ",
-                "*     ",
-                "*     ",
-                "*     "
+    // Create pattern maps
+    public static CharacterPatternMap[] createCharacterPatternMaps() {
+
+        String[] O = {
+            "   ***    ",
+			" **   **  ",
+			"**     ** ",
+			"**     ** ",
+			"**     ** ",
+			"**     ** ",
+			" **   **  ",
+			"   ***    "
         };
+
+        String[] P = {
+            "*****   ",
+            "**   ** ",
+            "**    **",
+            "**   ** ",
+            "*****   ",
+            "**      ",
+            "**      ",
+			"**      "
+        };
+
+        String[] S = {
+            "    ***** ",
+            "  **      ",
+            " **       ",
+            "  **      ",
+            "    ***   ",
+            "       ** ",
+			"        **",
+            "  ******  "
+        };
+
+        String[] space = {
+            "     ",
+            "     ",
+            "     ",
+            "     ",
+            "     ",
+			"     ",
+			"     ",
+			"     "
+        };
+
+        CharacterPatternMap[] maps = new CharacterPatternMap[4];
+        maps[0] = new CharacterPatternMap('O', O);
+        maps[1] = new CharacterPatternMap('P', P);
+        maps[2] = new CharacterPatternMap('S', S);
+        maps[3] = new CharacterPatternMap(' ', space);
+
+        return maps;
     }
 
-    // Method to generate S pattern
-    public static String[] getSPattern() {
-        return new String[]{
-                " *****",
-                "*     ",
-                "*     ",
-                " *****",
-                "     *",
-                "     *",
-                " *****"
-        };
+   
+    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
+
+        for (CharacterPatternMap map : charMaps) {
+            if (map.getCharacter() == ch) {
+                return map.getPattern();
+            }
+        }
+        return null;
     }
 
-    // Main method (entry point of program)
+
+    public static void printMessage(String message, CharacterPatternMap[] charMaps) {
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < message.length(); j++) {
+
+                char ch = message.charAt(j);
+                String[] pattern = getCharacterPattern(ch, charMaps);
+
+                System.out.print(pattern[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+ 
     public static void main(String[] args) {
 
-        String[] o = getOPattern();
-        String[] p = getPPattern();
-        String[] s = getSPattern();
+        CharacterPatternMap[] charMaps = createCharacterPatternMaps();
 
-        for (int i = 0; i < o.length; i++) {
-            System.out.println(
-                    o[i] + "   " +
-                    o[i] + "   " +
-                    p[i] + "   " +
-                    s[i]
-            );
-        }
+        String message = "OOPS";
+
+        printMessage(message, charMaps);
     }
 }
